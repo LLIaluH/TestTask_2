75a831ee809fe67b1275d04c54759109c4fd7b0e
LLIaluH <drunya2999@mail.ru>
2022-08-25 14:02:58 +0300

    Проверка Гит из VS

---
+++ TestTask_2/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTask_2.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public float Salary { get; set; }
+        public string DepartamentId { get; set; }
+        public string PCId { get; set; }
+    }
+}
